4ecd9e360a10a4e93e253be4a684089d7b3e6c4f
Paul Golmann <mail@pje-web.de>
2025-03-10 21:24:31 +0100

    Remove unused font, icons and logo

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,16 +1,5 @@
-import Apple from './apple.svelte';
-import Aria from './aria.svelte';
-import GitHub from './github.svelte';
-import Google from './google.svelte';
 import Hamburger from './hamburger.svelte';
 import Logo from './logo.svelte';
-import Npm from './npm.svelte';
-import PayPal from './paypal.svelte';
-import Pnpm from './pnpm.svelte';
-import RadixSvelte from './radix-svelte.svelte';
-import Tailwind from './tailwind.svelte';
-import Twitter from './twitter.svelte';
-import Yarn from './yarn.svelte';
 import ArrowRight from 'lucide-svelte/icons/arrow-right';
 import Check from 'lucide-svelte/icons/check';
 import ChevronLeft from 'lucide-svelte/icons/chevron-left';
@@ -58,23 +47,11 @@ export const Icons = {
   arrowRight: ArrowRight,
   help: CircleHelp,
   pizza: Pizza,
-  twitter: Twitter,
   check: Check,
   copy: Copy,
   copyDone: ClipboardCheck,
   sun: SunMedium,
   moon: Moon,
   laptop: Laptop,
-  gitHub: GitHub,
-  radix: RadixSvelte,
-  'Radix Svelte': RadixSvelte,
-  aria: Aria,
-  npm: Npm,
-  yarn: Yarn,
-  pnpm: Pnpm,
-  tailwind: Tailwind,
-  google: Google,
-  apple: Apple,
-  paypal: PayPal,
   Hamburger,
 };
